64b4068d448352cf79ef3682532254c7d56d5561
Izak Schmidlkofer <50844075+ischmidls@users.noreply.github.com>
2023-03-28 14:06:02 -0700

    Add bootstrap

--- pages/hegel/index.py
+++ pages/hegel/index.py
@@ -170,6 +170,7 @@ class CsvFigs:
         f.write('<hr>\n')
         f.write(fig.to_html(full_html=True, include_plotlyjs='cdn'))
         f.write('\n')
+        f.write('<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">')
         
   
   def all_figs(self):
