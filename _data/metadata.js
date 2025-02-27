d238f6eb14e9c3826adc0e8e47a4f62cc7eb72c5
Izak <izak.schmidlkofer@gmail.com>
2025-02-27 14:39:16 -0800

    gh-pages fix

---
+++ _data/metadata.js
@@ -0,0 +1,12 @@
+export default {
+	title: "izak",
+	url: "https://ischmidls.github.io",
+	language: "en",
+	description: "Learning through working",
+	author: {
+		name: "Izak Schmidlkofer",
+		email: "",
+		url: "https://ischmidls.github.io"
+	}
+}
+
---
+++ content/about.md
@@ -0,0 +1,9 @@
+---js
+const eleventyNavigation = {
+	key: "about",
+	order: 3
+};
+---
+# about
+
+Hi, I'm Izak, a math undergraduate with some experience in statistics, programming & tutoring.
